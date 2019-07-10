0c5a3204ae58b82ce68e2559a042a1ee833a8008
jchen4120 <jenniferchen@alumni.ubc.ca>
2019-07-10 00:49:46 -0700

    try to fix database agn

--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
 
 // this will be our data base's data structure
-var todoSchema = new Schema(
+var todoSchema = mongoose.Schema(
   {
     message: String
   }
